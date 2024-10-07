5535ad8fa08aaefd9438e622df57e2f53fdd9f95
lwhela12 <lucas@whelanpartners.com>
2024-10-06 21:10:21 -0700

    Prep for Heroku

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def chat():
     return jsonify({'response': response, 'terminate': False})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
 
 
